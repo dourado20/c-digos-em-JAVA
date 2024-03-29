021ded65b8d06fc15b0cd380ed72b1e0f39c6d8b
CLARA DOURADO <124412874+dourado20@users.noreply.github.com>
2024-03-29 18:59:07 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String nome;
+	    
+	    Scanner ler = new Scanner(System.in);
+	    
+		System.out.println("informe o nome");
+		 nome = ler.next();
+		
+		System.out.println("Bom Dia "+ nome);
+	}
+}
